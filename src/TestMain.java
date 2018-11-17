48b7956c4656c8045a12fe67935fcc327805ee80
sergik2907 <38475176+sergik2907@users.noreply.github.com>
2018-11-17 15:36:36 +0200

    Delete TestMain.java

--- src/TestMain.java
+++
@@ -1,9 +0,0 @@
-
-public class TestMain {
-
-	public static void main(String[] args) {
-		String name = "Sergey";
-		String hashedRequest = Hasher.hashRequest(name);
-		System.out.println(hashedRequest);
-	}
-}
