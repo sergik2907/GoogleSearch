384e37655c7da610bddb773b5899574d15e6f6d5
sergik2907 <38475176+sergik2907@users.noreply.github.com>
2018-11-20 20:33:46 +0200

    #1: Changed Logger and added function to get Request from Thread

--- src/RequestThread.java
+++ src/RequestThread.java
@@ -1,16 +1,29 @@
+import org.apache.log4j.Logger;
+
 public class RequestThread implements Runnable {
 
 	String request;
 	String fileName;
+	final static Logger LOGGER = Logger.getLogger(RequestThread.class);
 
 	public RequestThread(String request, String fileName) {
 		this.request = request;
 		this.fileName = fileName;
 	}
+	
+	public String getRequest() {
+		return this.request;
+	}
 
 	@Override
 	public void run() {
 		String content = RequestHelper.sendRequest(request);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			LOGGER.error(e);
+		}
 		RequestHelper.writeToFile(fileName, content);
+		LOGGER.info("done " + request);
 	}
 }
